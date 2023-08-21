3befc5f1975a34625730a8a7a6768d60132be801
Dyr3618 <120258634+Dyr3618@users.noreply.github.com>
2023-08-21 12:57:41 +0530

    Update DynamodbConnection.java

--- src/main/java/com/hellomsg/com/DynamodbConnection.java
+++ src/main/java/com/hellomsg/com/DynamodbConnection.java
@@ -6,21 +6,23 @@ import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.document.DynamoDB;
 import com.amazonaws.services.dynamodbv2.document.Item;
 import com.amazonaws.services.dynamodbv2.document.Table;
-public DynamodbConnection{
-    public void makeDBCall(){
-BasicAWSCredentials credentials = new BasicAWSCredentials("AKIAVRS5FCV2XRXVA2WE", "A0Y/e+HFCiuAZ1g6RCrjBybAnPAqKyyqIfbJKmaz");
-AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard()
-    .withCredentials(new AWSStaticCredentialsProvider(credentials))
-    .withRegion(Regions.US_EAST_1) // Replace with your desired region
-    .build();
-  DynamoDB dynamoDB = new DynamoDB(client);
-Table table = dynamoDB.getTable("TUser");
 
-// Put an item into the table
-Item item = new Item()
-    .withPrimaryKey("id", 123)
-    .withString("name", "John Doe");
+public class DynamodbConnection { // Added "class" keyword here
+    public void makeDBCall() {
+        BasicAWSCredentials credentials = new BasicAWSCredentials("AKIAVRS5FCV2XRXVA2WE", "A0Y/e+HFCiuAZ1g6RCrjBybAnPAqKyyqIfbJKmaz");
+        AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard()
+            .withCredentials(new AWSStaticCredentialsProvider(credentials))
+            .withRegion(Regions.US_EAST_1)
+            .build();
+        DynamoDB dynamoDB = new DynamoDB(client);
+        Table table = dynamoDB.getTable("TUser");
 
-table.putItem(item);
+        // Put an item into the table
+        Item item = new Item()
+            .withPrimaryKey("id", 123)
+            .withString("name", "John Doe");
+
+        table.putItem(item);
+    }
 }
-}
+
